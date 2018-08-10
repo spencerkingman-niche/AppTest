261e360c3f5b8ab7e22e6fe7b418dde00f5ee0d2
spencerkingman-niche <skingman@niche.com>
2018-08-10 09:06:45 -0400

    Start a data file

---
+++ app/data/QuizQuestions.js
@@ -0,0 +1,15 @@
+export const QuizData = {
+    questions: [
+        {
+            question: 'What language do you use with libraries such as React and Angular?',
+            answer1:'PHP', 
+            answer2:'JAVA',
+            answer3:'JavaScript',
+            answer4:'Python',
+            correctAnswer:'answer3',
+            key: '1A',
+        },
+        {
+        },
+    ]
+}
