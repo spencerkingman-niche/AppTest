0deee83c7390cd368b8761563f5b632e43f74fba
spencerkingman-niche <skingman@niche.com>
2018-08-10 09:56:07 -0400

    Create Questions

--- app/data/QuizQuestions.js
+++ app/data/QuizQuestions.js
@@ -10,6 +10,31 @@ export const QuizData = {
             key: '1A',
         },
         {
+            question: 'What day?',
+            answer1:'Sat', 
+            answer2:'Sun',
+            answer3:'Mon',
+            answer4:'Tues',
+            correctAnswer:'Mon',
+            key: '1B',
+        },
+        {
+            question: 'What name?',
+            answer1:'Spencer', 
+            answer2:'Hannah',
+            answer3:'Theda',
+            answer4:'Sidera',
+            correctAnswer:'Spencer',
+            key: '1C',
+        },
+        {
+            question: 'Where?',
+            answer1:'Pittsburgh', 
+            answer2:'Shadyside',
+            answer3:'PA',
+            answer4:'Earth',
+            correctAnswer:'answer4',
+            key: '1D',
         },
     ]
 }
